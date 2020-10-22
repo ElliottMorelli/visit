27f442a8fb25108870e81aa2cd45376cf50067bc
elliott <39219466+GMorelli4@users.noreply.github.com>
2020-10-22 09:49:06 -0400

    home controls

---
+++ home_controls.js
@@ -0,0 +1,6 @@
+const enter = document.getElementById('enter');
+    
+            enter.addEventListener('click', function() { 
+                window.location.href = "./doorways.html";
+    
+            });
